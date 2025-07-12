0e7379ed90c8291d4d1dad751332523e73b607cb
Everton J Souto Lima <everton.soutolima@mac.lan>
2025-07-12 22:58:36 +0200

    add deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
 
-import tailwindcss from '@tailwindcss/vite';
+const isProd = process.env.NODE_ENV === 'production';
 
-// https://astro.build/config
 export default defineConfig({
-  vite: {
-    plugins: [tailwindcss()]
-  }
+  site: isProd ? 'https://eejdev.github.io' : undefined,
+  base: isProd ? '/eejdev-blog/' : undefined,
+  output: 'static',
+  integrations: [tailwind()],
 });
+
