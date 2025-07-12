0c2c44a3fc520fe4a273ff2677d7701d7a7724ef
Everton J Souto Lima <everton.soutolima@mac.lan>
2025-07-12 22:44:31 +0200

    removed path

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,6 @@
 import { defineConfig } from 'astro/config';
 
-const username = 'eej-blog';
-const repo = 'eejdev-blog';
-
 export default defineConfig({
-  site: `https://${username}.github.io/${repo}`,
-  base: `/${repo}/`,
   output: 'static',
   vite: {
     plugins: [/* your existing tailwindcss plugin */],
